e7a071a35a7e3dd8b160dd12f3fb47d4aeeb5292
gautam00 <gautam.som774@gmail.com>
2017-06-08 19:17:55 +0600

    Add all files

---
+++ properties.js
@@ -0,0 +1,54 @@
+function callJSFunctions() {
+	
+	//Label/Text Color Change
+	document.getElementById('labelFontColor').addEventListener('change', function () {
+		document.getElementById('Main_Text').style.color = this.value;
+		document.getElementById('day').style.color = this.value;
+		document.getElementById('hr').style.color = this.value;
+		document.getElementById('min').style.color = this.value;
+		document.getElementById('sec').style.color = this.value;
+	});
+		
+	//Counter Color Change
+	document.getElementById('counterFontColor').addEventListener('change', function () {
+		document.getElementById('timer_days').style.color = this.value;
+		document.getElementById('timer_hours').style.color = this.value;
+		document.getElementById('timer_minutes').style.color = this.value;
+		document.getElementById('timer_seconds').style.color = this.value;
+	});
+		
+	//Counter Background Color Change
+	document.getElementById('counterBackgroundFontColor').addEventListener('change', function () {
+		document.getElementById('timer_days').style.backgroundColor = this.value;
+		document.getElementById('timer_hours').style.backgroundColor = this.value;
+		document.getElementById('timer_minutes').style.backgroundColor = this.value;
+		document.getElementById('timer_seconds').style.backgroundColor = this.value;
+	});	
+}
+
+function LabelFontSizeChange(){
+	document.getElementById('Main_Text').style.fontSize = document.getElementById('labelFontSize').value + "px";
+}
+
+function CounterFontSizeChange(){
+	document.getElementById('timer_days').style.fontSize = document.getElementById('counterFontSize').value + "px";
+	document.getElementById('timer_hours').style.fontSize = document.getElementById('counterFontSize').value + "px";
+	document.getElementById('timer_minutes').style.fontSize = document.getElementById('counterFontSize').value + "px";
+	document.getElementById('timer_seconds').style.fontSize = document.getElementById('counterFontSize').value + "px";
+}
+		
+function ImageUpload() {
+	document.getElementById('upload_image').addEventListener('change', readURL, true);
+	function readURL(){
+		var file = document.getElementById("upload_image").files[0];
+		var reader = new FileReader();
+		reader.onloadend = function(){
+			document.getElementById('background_image').style.backgroundImage = "url(" + reader.result + ")";        
+		}
+		if(file){
+			reader.readAsDataURL(file);
+		} else {
+					
+		}
+	}
+}
